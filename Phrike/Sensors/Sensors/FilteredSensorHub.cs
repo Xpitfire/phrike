58078d85dc36e899c404734e0f835af34442e828
Christian Neumüller <s1310307086@students.fh-hagenberg.at>
2015-05-08 08:50:10 +0200

    Sensors: Add filter interface.

---
+++ Phrike/Sensors/Sensors/FilteredSensorHub.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationPhrike.Sensors
+{
+    class FilteredSensorHub : ISensorHub
+    {
+
+        public IReadOnlyList<SensorInfo> Sensors
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsUpdating
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void SetSensorEnabled(SensorInfo sensor, bool enabled = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAvailableSampleCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetSensorValueIndexInSample(SensorInfo sensor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ISample> ReadSamples(int maxCount = int.MaxValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetFilter(SensorInfo sensor, IFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Phrike/Sensors/Sensors/IFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationPhrike.Sensors
+{
+    /// <summary>
+    /// Interface for sensor filters.
+    /// </summary>
+    public interface IFilter
+    {
+        /// <summary>
+        /// Hands <paramref name="unfilteredData"/> over for filtering.
+        /// </summary>
+        /// <param name="unfilteredData">The data to filter.</param>
+        /// <returns>
+        /// Filtered data. This can contain less elements
+        /// than <paramref name="unfilteredData"/>.
+        /// </returns>
+        IEnumerable<double> Filter(IEnumerable<double> unfilteredData);
+
+        /// <summary>
+        /// Signals the filter that the end of the is reached and filters any
+        /// remaining cached data points.
+        /// </summary>
+        /// <returns>Any remaining cached data points, filtered.</returns>
+        IEnumerable<double> Flush();
+
+        /// <summary>
+        /// Gets the radius of the filter.
+        /// </summary>
+        /// <remarks>
+        /// The filter requires one more data point than that to start
+        /// returning data.
+        /// </remarks>
+        int Radius { get; }
+    }
+}
