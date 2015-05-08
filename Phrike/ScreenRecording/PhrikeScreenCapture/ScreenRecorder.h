76c0a2f1e3d2a8ef74b5f80d6df4e18189b4d6ea
unknown <tom.neumair@gmail.com>
2015-05-08 09:29:32 +0200

    01.03 - Updated Screen Recorder comments

--- Phrike/ScreenRecording/PhrikeScreenCapture/ScreenRecorder.h
+++ Phrike/ScreenRecording/PhrikeScreenCapture/ScreenRecorder.h
@@ -9,16 +9,52 @@
 class PHRIKESCREENCAPTURE_API ScreenRecorder
 {
 public:
+	/**
+	 * Returns the Singleton instance of the Screen Recorder.
+	 * There can be only one instance of this class.
+	 */
 	static ScreenRecorder* getInstance();
+
+	/**
+	 * Destructor
+	 */
 	~ScreenRecorder();
 
+	/**
+	 * Starts the recording of the game window (incuding audio) and of the webcam.
+	 * @param directory: specifies the directory in which the files will be stored
+	 * @param gameFilename: specifies the filename of the game video (inlcuding extension)
+	 * @param cameraFilename: specifies the filename of the webcam video (including extension)
+     */
 	void startRecording(std::wstring directory = L"C:\\tmp\\", std::wstring gameFilename = L"game.mkv", std::wstring cameraFilename = L"camera.mkv");
+
+	/**
+	 * Stops the recording of the game window and of the webcam.
+	 */
 	void stopRecording();
 
+	/**
+	 * Starts the recording of the game window (including audio)
+	 * @param directory: specifies the directory in which the file will be stored
+	 * @param gameFilename: specifies the filename of the game video (inlcuding extension)
+	 */
 	void startGameRecording(std::wstring directory = L"C:\\tmp\\", std::wstring filename = L"game.mkv");
+
+	/**
+	 * Stops the recording of the game window.
+	 */
 	void stopGameRecording();
 
+	/**
+	* Starts the recording of the webcam
+	* @param directory: specifies the directory in which the file will be stored
+	* @param gameFilename: specifies the filename of the webcam video (inlcuding extension)
+	*/
 	void startCameraRecording(std::wstring directory = L"C:\\tmp\\", std::wstring filename = L"camera.mkv");
+
+	/**
+	* Stops the recording of the webcam.
+	*/
 	void stopCameraRecording();
 
 private:
