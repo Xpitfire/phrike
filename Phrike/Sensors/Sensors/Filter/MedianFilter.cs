37bbf186fa49f08a319bd5fa8ab7150fcd3ec515
Sandra Horner <sandra.horner93@gmx.at>
2015-05-21 16:16:33 +0200

    Filter: Add MedianFilter.

---
+++ Phrike/Sensors/Sensors/Filter/MedianFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationPhrike.Sensors
+{
+    class MedianFilter : FilterBase
+    {
+        public MedianFilter(int radius)
+            : base(radius)
+        {
+            //nothing to do
+        }
+
+        protected override double FilterData(int start, int end, int mid, double[] unfilteredData)
+        {
+            double[] sort = new double[end - start + 1];
+            Array.Copy(unfilteredData, start, sort, 0, end - start + 1);
+            Array.Sort(sort);
+
+            return sort[(end - start + 1) / 2];
+        }
+    }
+}
