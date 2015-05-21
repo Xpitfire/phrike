2d9b3522f5cfbb1683ed6e233f63ff4c8827b3b6
Josef Frauscher <s1310307058@students.fh-hagenberg.at>
2015-05-21 16:06:28 +0200

    Filter: Add PeakFilter and ValueDistanceFilter.

---
+++ Phrike/Sensors/Sensors/Filter/PeakFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationPhrike.Sensors.Filter
+{
+    public class PeakFilter : FilterBase
+    {
+
+        public PeakFilter(double threshold) : base (1)
+        {
+            Threshold = threshold;
+        }
+
+        protected override double FilterData(
+            int start, int end, int mid,
+            IReadOnlyList<double> unfilteredData)
+        {
+            double midVal = unfilteredData[mid];
+            double leftVal = start == mid ? double.MinValue : unfilteredData[start];
+            double rightVal = end == mid ? double.MinValue : unfilteredData[end];
+            if (midVal > Math.Max(leftVal, rightVal) && midVal >= Threshold)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        public double Threshold { get; private set; }
+
+    }
+}
---
+++ Phrike/Sensors/Sensors/Filter/ValueDistanceFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationPhrike.Sensors.Filter
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    public class ValueDistanceFilter
+    {
+        [SuppressMessage("ReSharper", "CompareOfFloatsByEqualityOperator")]
+        virtual public IList<double> Filter(IReadOnlyList<double> unfilteredData)
+        {
+            int cnt = 0;
+            bool peakFound = false;
+            var result = new List<double>();
+            for (int i = 0; i < unfilteredData.Count; i++)
+            {
+                if (unfilteredData[i] == 0.0)
+                {
+                    cnt++;
+                }
+                else if (unfilteredData[i] == 1.0)
+                {
+                    if (peakFound)
+                    {
+                        result.Add(cnt + 1);
+                    }
+                    peakFound = true;
+                    cnt = 0;
+                }
+            }
+            return result;
+        }
+    }
+}
