68a2f06e2cbaed645fd5829a86ba294460f8672b
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2015-05-21 16:11:39 +0200

    Check-in Windows Desktop Project due to library reference issues with Windows Store App Projekt.

---
+++ Phrike/GroundControl/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace Phrike.GroundControl
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
