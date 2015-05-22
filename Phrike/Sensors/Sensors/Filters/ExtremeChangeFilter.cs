49c7bd7f3c6d2efd93d7f41e1a73edc02ab7126c
Josef Frauscher <s1310307058@students.fh-hagenberg.at>
2015-05-22 10:38:21 +0200

    Filter: Add ExtremeChangeFilter.

---
+++ Phrike/Sensors/Sensors/Filters/ExtremeChangeFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationPhrike.Sensors.Filters
+{
+    public class ExtremeChangeFilter : FilterBase
+    {
+
+        private double lastValidVal;
+
+        public ExtremeChangeFilter(int lVV, double maxYDistance)
+            : base(1)
+        {
+            lastValidVal = lVV;
+            MaxYDistance = maxYDistance;
+        }
+
+        protected override double FilterData(int start, int end, int mid, IReadOnlyList<double> unfilteredData)
+        {
+
+            if (Math.Abs(unfilteredData[mid] - lastValidVal) > MaxYDistance)
+            {
+                return lastValidVal;
+            }
+            lastValidVal = unfilteredData[mid];
+            return unfilteredData[mid];
+
+        }
+
+        public double MaxYDistance { get; private set; }
+
+    }
+}
