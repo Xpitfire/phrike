f0853ea3d331fe3bd98a027b4c672ff030324aa6
Josef Frauscher <s1310307058@students.fh-hagenberg.at>
2015-05-29 11:09:01 +0200

    Filters: Add documentation.

--- Phrike/Sensors/Sensors/Filters/EdgeDetectionFilter.cs
+++ Phrike/Sensors/Sensors/Filters/EdgeDetectionFilter.cs
@@ -1,20 +1,37 @@
-﻿using System;
+﻿// <summary>Implements the EdgeDetectionFilter class.</summary>
+// -----------------------------------------------------------------------
+// Copyright (c) 2015 University of Applied Sciences Upper-Austria
+// Project OperationPhrike
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// -----------------------------------------------------------------------
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OperationPhrike.Sensors.Filters
 {
+    /// <summary>
+    /// Filter that intensivies peaks from the unfiltered data. 
+    /// </summary>
     public class EdgeDetectionFilter : RadiusFilterBase
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EdgeDetectionFilter" /> class.
+        /// </summary>
+        /// <param name="radius">Is passed to base constructor.</param>
         public EdgeDetectionFilter(int radius)
             : base(radius)
         {
             // nothing to do
         }
 
-
+        /// <inheritdoc/>
         protected override double FilterData(int start, int end, int mid, IReadOnlyList<double> unfilteredData)
         {
             int midFactor = end - start;
