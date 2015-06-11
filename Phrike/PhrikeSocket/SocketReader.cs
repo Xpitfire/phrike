2568ad3161c6ea5bc2147af5c81948a19fb1c627
dracc <alex.steinke@edumail.at>
2015-06-11 10:25:00 +0200

    Added the final version of the SocketWriter/-Reader Classes for GC-UE communication.

---
+++ Phrike/PhrikeSocket/SocketReader.cs
@@ -0,0 +1,123 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SocketReader.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the SocketReader type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Text;
+
+namespace Phrike.PhrikeSocket
+{
+    using System.IO;
+    using System.Net.Sockets;
+
+    /// <summary>
+    /// SocketReader to read data in a specific way from a given Socket.
+    /// Used for communication between UE and GC.
+    /// </summary>
+    public class SocketReader
+    {
+        private byte[] buffer;
+        private Socket socket;
+        private MemoryStream ms;
+        private int length;
+
+        private bool readAble;
+        private bool receiveAble;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SocketReader"/> class.
+        /// </summary>
+        /// <param name="socket">
+        /// The socket to receive data from.
+        /// </param>
+        public SocketReader(Socket socket)
+        {
+            this.socket = socket;
+
+            this.receiveAble = true;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the reader is ready to receive data from the Socket.
+        /// </summary>
+        public bool CanReceive
+        {
+            get
+            {
+                return this.receiveAble;
+            }
+        }
+        
+        /// <summary>
+        /// Gets a value indicating whether the reader is ready to read data from a received byte-stream.
+        /// </summary>
+        public bool CanRead
+        {
+            get
+            {
+                return this.readAble;
+            }
+        }
+
+        /// <summary>
+        /// Reads a String from the already received Byte-Stream.
+        /// </summary>
+        /// <exception cref="Exception">Throws an exception when the Reader is not ready to read.</exception>
+        /// <returns>The next String that can be read from the buffer</returns>
+        public string ReadString()
+        {
+            if (!this.CanRead)
+            {
+                throw new Exception("Reader not ready to read!");
+            }
+
+            int strlen = this.ms.ReadByte();
+
+            byte[] buf2 = new byte[strlen];
+            this.ms.Read(buf2, 0, strlen);
+
+            string ret = Encoding.UTF8.GetString(buf2, 0, strlen);
+            return ret;
+        }
+
+        /// <summary>
+        /// Reads a float from the already received Byte-Stream.
+        /// </summary>
+        /// <exception cref="Exception">Throws an exception when the Reader is not ready to read.</exception>
+        /// <returns>The next float that can be read from the buffer</returns>
+        public float ReadFloat()
+        {
+            if (!this.CanRead)
+            {
+                throw new Exception("Reader not ready to read!");
+            }
+
+            byte[] buf2 = new byte[4];
+            this.ms.Read(buf2, 0, 4);
+
+            float ret = BitConverter.ToSingle(buf2, 0);
+            return ret;
+        }
+
+        /// <summary>
+        /// Receives a new byte-sausage from the Socket and readies the Reader for reading.
+        /// </summary>
+        public void Receive()
+        {
+            if (!this.CanReceive)
+            {
+                throw new Exception("Reader not ready to receive!");
+            }
+
+            this.buffer = new byte[100];
+            this.length = this.socket.Receive(this.buffer);
+            this.ms = new MemoryStream(this.buffer, 0, this.length);
+            this.readAble = true;
+        }
+    }
+}
---
+++ Phrike/PhrikeSocket/SocketWriter.cs
@@ -0,0 +1,145 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SocketWriter.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the SocketWriter type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Text;
+
+namespace Phrike.PhrikeSocket
+{
+    using System.IO;
+    using System.Net.Sockets;
+
+    /// <summary>
+    /// SocketWriter to write data in a specific way to a given Socket.
+    /// Used for communication between UE and GC.
+    /// </summary>
+    public class SocketWriter
+    {
+        private byte[] buffer;
+        private Socket socket;
+        private MemoryStream ms;
+
+        private bool writeAble;
+        private bool sendAble;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SocketWriter"/> class. 
+        /// </summary>
+        /// <param name="socket">
+        /// The Socket to send data to.
+        /// </param>
+        public SocketWriter(Socket socket)
+        {
+            this.Init();
+            this.socket = socket;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the writer is ready to write data to a the buffer. 
+        /// </summary>
+        public bool CanWrite
+        {
+            get
+            {
+                return this.writeAble;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the writer is ready to write the buffer to the socket.
+        /// </summary>
+        public bool CanSend
+        {
+            get
+            {
+                return this.sendAble;
+            }
+        }
+
+        /// <summary>
+        /// Writes a String to the buffer.
+        /// </summary>
+        /// <param name="text">
+        /// The string to write to the buffer.
+        /// </param>
+        /// <exception cref="Exception">
+        /// Throws an exception when the Reader is not ready to write.
+        /// </exception>
+        public void WriteString(string text)
+        {
+            if (!this.CanWrite)
+            {
+                throw new Exception("Writer not ready to Write!");
+            }
+
+            byte[] byteStr = Encoding.UTF8.GetBytes(text);
+
+            this.ms.WriteByte((byte)byteStr.Length);
+            this.ms.Write(byteStr, 0, byteStr.Length);
+
+            if (!this.sendAble)
+            {
+                this.sendAble = true;
+            }
+        }
+
+        /// <summary>
+        /// Writes a float to the buffer.
+        /// </summary>
+        /// <param name="value">
+        /// The float to write to the buffer.
+        /// </param>
+        /// <exception cref="Exception">
+        /// Throws an exception when the Writer is not ready to write.
+        /// </exception>
+        public void WriteFloat(float value)
+        {
+            if (!this.CanWrite)
+            {
+                throw new Exception("Writer not ready to Write!");
+            }
+
+            byte[] bval = BitConverter.GetBytes(value);
+
+            this.ms.Write(bval, 0, bval.Length);
+
+            if (!this.sendAble)
+            {
+                this.sendAble = true;
+            }
+        }
+
+        /// <summary>
+        /// Sends the buffer as a byte-sausage to the Socket and reinitialises the Buffer.
+        /// </summary>
+        public void Send()
+        {
+            if (!this.CanSend)
+            {
+                throw new Exception("Writer not ready to send!");
+            }
+
+            this.socket.Send(this.buffer, Convert.ToInt32(this.ms.Position), SocketFlags.None);
+            this.ms.Close();
+
+            this.sendAble = false;
+            this.Init();
+        }
+
+        /// <summary>
+        /// (Re)initialisies the buffer and the MemoryStream.
+        /// </summary>
+        private void Init()
+        {
+            this.buffer = new byte[100];
+            this.ms = new MemoryStream(this.buffer);
+            this.writeAble = true;
+        }
+    }
+}
