db59ca62ae760de0bf1a89359c454a060310c29c
Wolfgang Mayr <s1410307080@students.fh-hagenberg.at>
2015-10-05 16:08:36 +0200

    DataModel added

---
+++ Phrike/DataAccess/OperationPhrikeContext.cs
@@ -0,0 +1,29 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class OperationPhrikeContext : DbContext
+    {
+        DbSet<PositionData> PositionData;
+        DbSet<Survey> Surveys;
+        DbSet<SurveyQuestion> SurveyQuestions;
+        DbSet<SurveyResult> SurveyResults;
+        DbSet<Test> Tests;
+        DbSet<Propositus> Propositi;
+        DbSet<Video> Videos;
+        
+        
+        public Propositus GetDeineMudda ()
+        {
+            return (from m in this.Propositi
+                    where m.LastName == "Steinke" && m.Sex == Sex.Female
+                    select m).SingleOrDefault();
+        }
+    }
+}
---
+++ Phrike/DataModel/PositionData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class PositionData : BaseEntity
+    {
+        public DateTime Time { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+
+        // TODO: RIchtig anordnen
+        public float Pitch { get; set; }
+        public float Yaw { get; set; }
+        public float Roll { get; set; }
+    }
+}
