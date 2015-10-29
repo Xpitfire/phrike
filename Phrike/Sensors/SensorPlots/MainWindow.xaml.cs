Xpitfire/phrike

commit 009871670480e69f9f70ef1f49213692a2515bc7

SensorPlots: Do not prefilter raw data.

Phrike/Sensors/SensorPlots/MainWindow.xaml.cs
@@ -168,8 +168,7 @@ namespace Phrike.SensorPlots
 
             var filterChain = new FilterChain();
 
-            filterChain.Add(new GaussFilter(4));
-            filterChain.Add(new EdgeDetectionFilter(2));
+         
             IReadOnlyList<double> prefilteredData = filterChain.Filter(sensorData);
             
             if (this.Checkbox.IsChecked == true)
@@ -187,6 +186,8 @@ namespace Phrike.SensorPlots
                 this.mergedPeaksSeries.Points.Clear();
                 this.pulseSeries.Points.Clear();
                 this.trendSeries.Points.Clear();
+                filterChain.Add(new GaussFilter(4));
+                filterChain.Add(new EdgeDetectionFilter(2));
 
                 IReadOnlyList<double> maxPeaks = new PeakFilter(15).Filter(prefilteredData);
                 var maxFilter = new BinaryThresholdFilter(0.5);
@@ -240,7 +241,7 @@ namespace Phrike.SensorPlots
         /// </summary>
         /// <param name="sender">The event source.</param>
         /// <param name="e">Additional event arguments.</param>
-        private void CheckBox_Changed(object sender, RoutedEventArgs e)
+        private void CheckBoxChanged(object sender, RoutedEventArgs e)
         {
             this.UpdateMainWindow();
         }

Phrike/Sensors/SensorPlots/MainWindow.xaml
@@ -35,7 +35,7 @@
             <Button Margin="5" Click="BtnOpenFileClicked">Open file…</Button>
             <TextBlock Margin="5">Display Channel:</TextBlock>
             <ComboBox x:Name="ChannelSelection" Margin="5" SelectionChanged="CbChannelSelected"/>
-            <CheckBox x:Name="Checkbox" Content="Show Raw Data" HorizontalAlignment="Left" Margin="10,10,0,0" VerticalAlignment="Top" Checked="CheckBox_Changed" Unchecked="CheckBox_Changed" />
+            <CheckBox x:Name="Checkbox" Content="Show Raw Data" HorizontalAlignment="Left" Margin="10,10,0,0" VerticalAlignment="Top" Checked="CheckBoxChanged" Unchecked="CheckBoxChanged" />
         </StackPanel>
         
     </Grid>

commit 3052189a492c9975e8a118dc305bee3de0965d0d

SensorPlots: Fix data not being prefilterd.
Regression from 009871670480e69f9f70ef1f49213692a2515bc7.

Phrike/Sensors/SensorPlots/MainWindow.xaml.cs
@@ -169,16 +169,13 @@ namespace Phrike.SensorPlots
             int sensorIdx = this.dataSource.GetSensorValueIndexInSample(sensor);
             double[] sensorData = SensorUtil.GetSampleValues(this.data, sensorIdx).ToArray();
 
-            var filterChain = new FilterChain();
-
-            IReadOnlyList<double> prefilteredData = filterChain.Filter(sensorData);
             
             if (this.Checkbox.IsChecked == true)
             {
                 for (int i = 0; i < sensorData.Length; ++i)
                 {
                     var x = i / (double)this.dataSource.SampleRate;
-                    this.dataSeries.Points.Add(new DataPoint(x, prefilteredData[i]));
+                    this.dataSeries.Points.Add(new DataPoint(x, sensorData[i]));
                 }
             }
             else
@@ -188,9 +185,12 @@ namespace Phrike.SensorPlots
                 this.mergedPeaksSeries.Points.Clear();
                 this.pulseSeries.Points.Clear();
                 this.trendSeries.Points.Clear();
-                filterChain.Add(new GaussFilter(4));
-                filterChain.Add(new EdgeDetectionFilter(2));
+                var filterChain = new FilterChain(
+                    new GaussFilter(4),
+                    new EdgeDetectionFilter(2)
+                );
 
+                IReadOnlyList<double> prefilteredData = filterChain.Filter(sensorData);
                 IReadOnlyList<double> maxPeaks = new PeakFilter(15).Filter(prefilteredData);
                 var maxFilter = new BinaryThresholdFilter(0.5);
                 maxPeaks = maxFilter.Filter(maxPeaks);