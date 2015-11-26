5e65a476f376a3e490c185ef91fd9faf31f182b7
Christian Neumüller <cn00@gmx.at>
2015-11-26 10:25:34 +0100

    DataModel: Add AuxiliaryDataMimeTypes constants.

---
+++ Phrike/DataModel/AuxiliaryDataMimeTypes.cs
@@ -0,0 +1,24 @@
+﻿// <summary></summary>
+// -----------------------------------------------------------------------
+// Copyright (c) 2015 University of Applied Sciences Upper-Austria
+// Project OperationPhrike
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// -----------------------------------------------------------------------
+namespace DataModel
+{
+    /// <summary>
+    /// Defines string constants for use as <see cref="AuxilaryData.MimeType"/>.
+    /// </summary>
+    public static class AuxiliaryDataMimeTypes
+    {
+        public const string Biofeedback2000Csv= "application/x-biofeedback200";
+        public const string GMobilabPlusBin = "application/x-gmobilabplusbin";
+    }
+}
