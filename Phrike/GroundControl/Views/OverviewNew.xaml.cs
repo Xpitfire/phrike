5f218a89679a5c6acb6d22fd6b628b1f3b54dd95
dracc <alex.steinke@edumail.at>
2015-11-30 14:13:55 +0100

    New, real ViewModels added, also integrated in User- and ScenarioSelect.

---
+++ Phrike/GroundControl/Views/OverviewNew.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Phrike.GroundControl.ViewModels;
+
+namespace Phrike.GroundControl.Views
+{
+    /// <summary>
+    /// Interaction logic for OverviewNew.xaml
+    /// </summary>
+    public partial class OverviewNew : UserControl
+    {
+        public OverviewNew()
+        {
+            InitializeComponent();
+
+            this.Loaded += (sender, args) =>
+            {
+                this.DataContext = new OverviewVM();
+            };
+        }
+
+        private void SelectNewSubject(object sender, RoutedEventArgs e)
+        {
+            // TODO: call UserSelect uc
+        }
+
+        private void SelectNewScenario(object sender, RoutedEventArgs e)
+        {
+            // TODO: call ScenarioSelect uc        
+        }
+    }
+}
