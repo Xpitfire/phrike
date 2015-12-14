5ab321673bc628adadae776b79f174f50388ea87
Christian Neumüller <cn00@gmx.at>
2015-12-14 15:05:44 +0100

    GroundControl: Add PathToImageConverter.

---
+++ Phrike/GroundControl/PathToImageConverter.cs
@@ -0,0 +1,55 @@
+﻿// <summary></summary>
+// -----------------------------------------------------------------------
+// Copyright (c) 2015 University of Applied Sciences Upper-Austria
+// Project OperationPhrike
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR 
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// -----------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Phrike.GroundControl
+{
+    // Adapted from http://stackoverflow.com/a/1763678/2128694
+    // Required because WPF's default string to image source converter leaves
+    // the file open and FileStorageHelper.SetSubjectAvatar, etc. fail then.
+    public class PathToImageConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            var path = value as string;
+            if (path != null && !path.StartsWith("pack://"))
+            {
+                var image = new BitmapImage();
+                using (FileStream stream = File.OpenRead(path))
+                {
+                    image.BeginInit();
+                    image.StreamSource = stream;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.EndInit(); // load the image from the stream
+                } // close the stream
+                return image;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
