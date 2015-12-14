7e281251f359899b978d95d9a814374850a3eee0
Christian Neumüller <cn00@gmx.at>
2015-12-14 15:31:49 +0100

    Split AuxDataVm from AuxDataListVm.

--- Phrike/GroundControl/ViewModels/AuxiliaryDataListViewModel.cs
+++ Phrike/GroundControl/ViewModels/AuxiliaryDataListViewModel.cs
@@ -78,29 +78,4 @@ namespace Phrike.GroundControl.ViewModels
             AuxiliaryData.Add(new AuxiliaryDataViewModel(data));
         }
     }
-
-    public class AuxiliaryDataViewModel
-    {
-        public AuxiliaryDataViewModel(AuxilaryData model)
-        {
-            this.Model = model;
-        }
-
-        public string DisplayName
-            =>
-                string.IsNullOrEmpty(Model.Description)
-                    ? Model.FilePath
-                    : Model.Description;
-
-        public string FullInfo => "Datei: " + Model.FilePath + "\nTyp: " + Model.MimeType;
-
-        public string CategoryName
-            =>
-                AuxiliaryDataMimeTypes.GetCategory(Model.MimeType)
-                == AuxiliaryDataMimeTypes.Category.Video
-                    ? "Video"
-                    : "Sensoraufzeichnung";
-
-        public AuxilaryData Model { get; }
-    }
 }
---
+++ Phrike/GroundControl/ViewModels/AuxiliaryDataViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DataModel;
+
+namespace Phrike.GroundControl.ViewModels
+{
+    public class AuxiliaryDataViewModel
+    {
+        public AuxiliaryDataViewModel(AuxilaryData model)
+        {
+            this.Model = model;
+        }
+
+        public string DisplayName
+            =>
+                string.IsNullOrEmpty(Model.Description)
+                    ? Model.FilePath
+                    : Model.Description;
+
+        public string FullInfo => "Datei: " + Model.FilePath + "\nTyp: " + Model.MimeType;
+
+        public string CategoryName
+            =>
+                AuxiliaryDataMimeTypes.GetCategory(Model.MimeType)
+                == AuxiliaryDataMimeTypes.Category.Video
+                    ? "Video"
+                    : "Sensoraufzeichnung";
+
+        public AuxilaryData Model { get; }
+    }
+}
