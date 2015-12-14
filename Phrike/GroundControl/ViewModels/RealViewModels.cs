5d4da86db7ecfef8631ab4f0ee3c1acf540c3016
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2015-12-14 15:44:20 +0100

    Added viewmodels for test and added implementation for test archive view.

--- Phrike/GroundControl/ViewModels/RealViewModels.cs
+++ Phrike/GroundControl/ViewModels/RealViewModels.cs
@@ -5,12 +5,124 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data.Entity.Validation;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using DataAccess;
+using Phrike.GroundControl.Annotations;
 using Phrike.GroundControl.Helper;
 
 namespace Phrike.GroundControl.ViewModels
 {
+    class TestCollectionVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<TestVM> Tests { get; set; }
+        private TestVM currentTest;
+
+        public TestCollectionVM()
+        {
+            Tests = new ObservableCollection<TestVM>();
+            currentTest = new TestVM();
+            if (DataLoadHelper.IsLoadDataActive())
+                LoadTest();
+        }
+
+        private async void LoadTest()
+        {
+            Tests.Clear();
+
+            using (var x = new UnitOfWork())
+            {
+                IEnumerator<Test> enu = x.TestRepository.Get(includeProperties: "Scenario,Subject").GetEnumerator();
+                while (await Task.Factory.StartNew(() => enu.MoveNext()))
+                {
+                    Tests.Add(new TestVM(enu.Current));
+                }
+            }
+        }
+
+        public TestVM CurrentTest
+        {
+            get { return this.currentTest; }
+            set
+            {
+                if (currentTest != value)
+                {
+                    this.currentTest = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentTest)));
+                }
+            }
+        }
+    }
+
+    internal class TestVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Test test;
+        
+        public Test Test
+        {
+            get { return test; }
+            set
+            {
+                test = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Test)));
+            }
+        }
+
+        public TestVM(Test test)
+        {
+            this.test = test;
+        }
+
+        public TestVM()
+        {
+            test = new Test();
+        }
+
+        public string FullTitle => $"{test.Title} {test.Subject?.LastName} {test.Scenario?.Name}";
+        
+        public string Title
+        {
+            get { return test.Title; }
+            set
+            {
+                if (test.Title != value)
+                {
+                    test.Title = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+                }
+            }
+        }
+
+        public Scenario Scenario
+        {
+            get { return test.Scenario; }
+            set
+            {
+                if (test.Scenario != value)
+                {
+                    test.Scenario = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Scenario)));
+                }
+            }
+        }
+
+        public Subject Subject
+        {
+            get { return test.Subject; }
+            set
+            {
+                if (test.Subject != value)
+                {
+                    test.Subject = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Subject)));
+                }
+            }
+        }
+
+    }
+
     class SubjectCollectionVM : INotifyPropertyChanged
     {
         private SubjectVM currentSubject;
