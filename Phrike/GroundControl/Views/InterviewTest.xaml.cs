9094314dad7c4aca575d6b1dd32e03cc18260622
Sandra Horner <sandra.horner93@gmx.at>
2015-12-14 15:54:50 +0100

    GroundControl:  Added Functions to get the Interviewresult

--- Phrike/GroundControl/Views/InterviewTest.xaml.cs
+++ Phrike/GroundControl/Views/InterviewTest.xaml.cs
@@ -9,9 +9,11 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using DataAccess;
 using DataModel;
 
 namespace Phrike.GroundControl.Views
@@ -25,12 +27,9 @@ namespace Phrike.GroundControl.Views
         /// Initializes a new instance of the <see cref="InterviewTest"/> class.
         /// </summary>
         public InterviewTest()
-        {
-            AnswerList = new List<SurveyAnswer>();
-            InitializeComponent();
+        {        
+            this.InitializeComponent();
         }
-
-        public List<SurveyAnswer> AnswerList { get; set; }
 
         /// <summary>
         /// The button_ click.
@@ -43,8 +42,18 @@ namespace Phrike.GroundControl.Views
         /// </param>
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
-           // this.AnswerList = this.GetInterviewResult();
-           this.GetInterviewResult();
+            this.SaveData();
+        }
+
+        private List<RadioButton> GetRadioButtons()
+        {
+            var grid = InterviewGrid;
+            List<RadioButton> result = new List<RadioButton>();
+            foreach(RadioButton rb in FindVisualChildren<RadioButton>(grid))
+            {
+                result.Add(rb);
+            }
+            return result;
         }
 
         /// <summary>
@@ -53,14 +62,91 @@ namespace Phrike.GroundControl.Views
         /// <returns>
         /// The <see cref="List"/>.
         /// </returns>
-        private void GetInterviewResult()
+        public List<SurveyResult> GetInterviewResult()
         {
-            var grid = InterviewGrid;
-            List<SurveyAnswer> result = new List<SurveyAnswer>();
+            List<SurveyResult> result = new List<SurveyResult>();
+            List<RadioButton> radioButtons = new List<RadioButton>(this.GetRadioButtons());
             int i = 1;
-            foreach (RadioButton rb in FindVisualChildren<RadioButton>(grid))
+
+            foreach (RadioButton rb in radioButtons)
             {
-                Console.WriteLine(rb.IsChecked);
+                if (rb.IsChecked == true)
+                {
+                    SurveyResult s = new SurveyResult();
+                    int resultValue = i % 5;
+                    if (resultValue == 0)
+                    {
+                        resultValue = 5;
+                    }
+
+                    switch (resultValue)
+                    {
+                        case 1:
+                            s.Answer = SurveyAnswer.Perfect; 
+                            break;
+                        case 2:
+                            s.Answer = SurveyAnswer.Good;
+                            break;
+                        case 3:
+                            s.Answer = SurveyAnswer.Gratifying;
+                            break;
+                        case 4:
+                            s.Answer = SurveyAnswer.Bad;
+                            break;
+                        case 5:
+                            s.Answer = SurveyAnswer.Worst;
+                            break;
+                    }
+
+                    result.Add(s);
+                }
+
+                i++;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// The save data.
+        /// </summary>
+        private void SaveData()
+        {
+            using (UnitOfWork unitOfWork = new UnitOfWork())
+            {
+                int i = 0; // !!!!
+                List<SurveyResult> resultList = new List<SurveyResult>(this.GetInterviewResult());
+
+                SurveyQuestion[] questionList = unitOfWork.SurveyQuestionRepository.Get().ToArray();
+                List<SurveyQuestion> questionList2 = (List<SurveyQuestion>)unitOfWork.SurveyQuestionRepository.Get();
+                Test test = unitOfWork.TestRepository.GetByID(2);
+
+                foreach (var q in questionList2)
+                {
+                    Console.WriteLine(q.Question);
+                }
+
+                for (int j = 0; j < questionList.Length; j++)
+                {
+                    Console.WriteLine(questionList[j]);
+                }
+
+                foreach (SurveyResult result in resultList)
+                {
+                    result.Test = test;
+                    result.SurveyQuestion = questionList[i];
+                    unitOfWork.SurveyResultRepository.Insert(result);
+                    //  unitOfWork.Save();
+
+                    Console.WriteLine(
+                        "inserted result {0}, {1}, {2}",
+                        result.SurveyQuestion,
+                        result.Answer,
+                        result.Test);
+                    i++;
+                    //}
+
+
+                }
             }
         }
 
@@ -82,10 +168,11 @@ namespace Phrike.GroundControl.Views
                 for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
                 {
                     DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
-                    if (child != null && child is T)
+                    if ((child != null) && (child is T))
                     {
                         yield return (T)child;
                     }
+
                     foreach (T childOfChild in FindVisualChildren<T>(child))
                     {
                         yield return childOfChild;
@@ -93,24 +180,5 @@ namespace Phrike.GroundControl.Views
                 }
             }
         }
-
-        /// <summary>
-        /// The radio button_ checked.
-        /// </summary>
-        /// <param name="sender">
-        /// The sender.
-        /// </param>
-        /// <param name="e">
-        /// The e.
-        /// </param>
-        //private void RadioButtonChecked(object sender, RoutedEventArgs e)
-        //{          
-        //    RadioButton rb = (RadioButton)sender;
-
-        //    if (rb.IsChecked == true)
-        //    {
-        //        Console.WriteLine(rb.IsChecked + "1");
-        //    }
-        //}
     }
 }
