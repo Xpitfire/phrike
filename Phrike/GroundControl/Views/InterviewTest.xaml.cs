7482948da5b9c680d85e7b8b8672b25934306817
Sandra Horner <sandra.horner93@gmx.at>
2015-12-14 13:00:04 +0100

    Ground Control: Add DatabaseConnection to InterviewTest

--- Phrike/GroundControl/Views/InterviewTest.xaml.cs
+++ Phrike/GroundControl/Views/InterviewTest.xaml.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="InterviewTest.xaml.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for InterviewTest.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using DataModel;
+
 namespace Phrike.GroundControl.Views
 {
     /// <summary>
@@ -15,8 +26,11 @@ namespace Phrike.GroundControl.Views
         /// </summary>
         public InterviewTest()
         {
-            this.InitializeComponent();
+            AnswerList = new List<SurveyAnswer>();
+            InitializeComponent();
         }
+
+        public List<SurveyAnswer> AnswerList { get; set; }
 
         /// <summary>
         /// The button_ click.
@@ -29,16 +43,74 @@ namespace Phrike.GroundControl.Views
         /// </param>
         private void ButtonClick(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine(@"hallo");
+           // this.AnswerList = this.GetInterviewResult();
+           this.GetInterviewResult();
         }
 
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// The get interview result.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
+        private void GetInterviewResult()
         {
-            RadioButton rb = (RadioButton)sender;
-            if (rb.IsChecked == true)
+            var grid = InterviewGrid;
+            List<SurveyAnswer> result = new List<SurveyAnswer>();
+            int i = 1;
+            foreach (RadioButton rb in FindVisualChildren<RadioButton>(grid))
             {
                 Console.WriteLine(rb.IsChecked);
             }
         }
+
+        /// <summary>
+        /// The find visual children.
+        /// </summary>
+        /// <param name="depObj">
+        /// The dep obj.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
+        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {               
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The radio button_ checked.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        //private void RadioButtonChecked(object sender, RoutedEventArgs e)
+        //{          
+        //    RadioButton rb = (RadioButton)sender;
+
+        //    if (rb.IsChecked == true)
+        //    {
+        //        Console.WriteLine(rb.IsChecked + "1");
+        //    }
+        //}
     }
 }
