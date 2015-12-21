8600125b79460a41aea45f28c17a756a3a6f3728
Josef Frauscher <s1310307058@students.fh-hagenberg.at>
2015-12-21 10:30:26 +0100

    Add DialogHelper class that allows showing dialogs to the user.

---
+++ Phrike/GroundControl/Helper/DialogHelper.cs
@@ -0,0 +1,23 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Phrike.GroundControl.Helper
+{
+    public class DialogHelper
+    {
+        public async void ShowDialogAsync(string title, string message, MessageDialogStyle dialogStyle = MessageDialogStyle.Affirmative)
+        {
+            await ShowDialog(title, message, dialogStyle);
+        }
+
+        private async Task<MessageDialogResult> ShowDialog(string title, string message, MessageDialogStyle dialogStyle)
+        {
+            var metroWindow = (Application.Current.MainWindow as MetroWindow);
+            metroWindow.MetroDialogOptions.ColorScheme = MetroDialogColorScheme.Accented;
+
+            return await metroWindow.ShowMessageAsync(title, message, dialogStyle, metroWindow.MetroDialogOptions);
+        }
+    }
+}
